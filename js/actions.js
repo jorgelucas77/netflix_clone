a1c34fc75917e9ff66286946153812a7d6289bba
Jorge Lucas <jorgelucas77@gmail.com>
2020-08-28 15:46:00 +0200

    Código inicial

---
+++ js/actions.js
@@ -0,0 +1,11 @@
+function maisinfo(){
+     window.open("http://www.adorocinema.com/series/serie-21504/");
+  }
+
+  function trailer(){
+  //  window.open("http://player.adorocinema.com/19557595.html");
+ 
+    var myWindow = window.open("", "Trailer", "width=490,height=350");
+    myWindow.document.write("<div id='blogvision'><iframe src='http://player.adorocinema.com/19557595.html' style='width:480px; height:270px'>   </iframe>    <br />     </div>");
+  }
+ 
